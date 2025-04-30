44efbb458b39dc5a7bbf922e06e085ea13e7e35e
Usman Muhammad <68919810+musman65@users.noreply.github.com>
2025-04-30 17:47:03 -0400

    added java doc to enemy class

--- RPG_Project/src/main/java/org/example/enemy_and_sub/Enemy.java
+++ RPG_Project/src/main/java/org/example/enemy_and_sub/Enemy.java
@@ -15,21 +15,32 @@ public class Enemy extends Player implements MoveStrategy {
         super(name, maxHealth, health);
     }
 
-    public Enemy(String name, float health, float maxHealth, List<Move> moves) {
-        super(name, health, maxHealth);
-        this.moves = moves;
-    }
-
+    /**
+     * Selects a move strategy for the enemy to base itself upon when choosing a move
+     * @param enemy the enemy that is using the move
+     * @param human the user
+     * @return wisest move choice
+     */
     @Override
     public Move selectMoveStrategy(Enemy enemy, Human human) {
         return null;
     }
 
+    /**
+     * Takes damage by removing health from the health field by taking weaknesses into account
+     * @param damage the damage dealt (how much should be removed)
+     * @param moveType the type of move that was used
+     */
     @Override
     public void takeDamage(int damage, Move.Type moveType) {
 
     }
 
+    /**
+     * Chooses a move for the enemy based on the conditions (strategy)
+     * @param player the player that the move will be used on
+     * @return a Move to use for the enemy
+     */
     @Override
     public Move choseMove(Player player) {
         return null;
